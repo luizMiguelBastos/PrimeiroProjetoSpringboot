cb925d63540f8d14cb39faff0f1c9fb3dac52055
Luiz Miguel Bastos <luizmiguelbastos90@gmail.com>
2026-08-03 15:24:07 -0300

    Lombok adicionado ao projeto para reduzir codigos redundantes

--- src/main/java/br/com/LuizMiguel/firstProject/user/UserModel.java
+++ src/main/java/br/com/LuizMiguel/firstProject/user/UserModel.java
@@ -1,34 +1,13 @@
 package br.com.LuizMiguel.firstProject.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
 
       private String username;
       private String name;
       private String password;
-
-      //getters
-     public String getUsername() {
-          return username;
-     }
-     public String getName() {
-          return name;
-     }
-     public String getPassword() {
-          return password;
-     }
-
-     //setters
-     public void setUsername(String username) {
-          this.username = username;
-     }
-     public void setName(String name) {
-          this.name = name;
-     }
-     public void setPassword(String password) {
-          this.password = password;
-     }
-
-
 
 }
 
