77bdce56780c45b208dd61737ea222dd46540747
Luiz Miguel Bastos <luizmiguelbastos90@gmail.com>
2026-08-04 18:18:56 -0300

    Criando uma entidade e chave primaria para o H2 Database

--- src/main/java/br/com/LuizMiguel/firstProject/user/UserModel.java
+++ src/main/java/br/com/LuizMiguel/firstProject/user/UserModel.java
@@ -1,9 +1,19 @@
 package br.com.LuizMiguel.firstProject.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
+import java.util.UUID;
+
 @Data
+@Entity (name = "tb_users")
 public class UserModel {
+
+      @Id
+      @GeneratedValue (generator = "UUID")
+      private UUID id;
 
       private String username;
       private String name;
