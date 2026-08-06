a68898bfd33b19f0c3bfe6c4641ca93d031fb876
Luiz Miguel Bastos <luizmiguelbastos90@gmail.com>
2026-08-06 20:27:10 -0300

    Implementando funcionalidades de validacao das horas cadastradas

--- src/main/java/br/com/LuizMiguel/firstProject/task/TaskController.java
+++ src/main/java/br/com/LuizMiguel/firstProject/task/TaskController.java
@@ -2,11 +2,14 @@ package br.com.LuizMiguel.firstProject.task;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @RestController
@@ -17,11 +20,21 @@ public class TaskController {
     private ITaskReposity taskReposity;
 
     @PostMapping("/")
-    public TaskModel create (@RequestBody TaskModel taskModel, HttpServletRequest request){
+    public ResponseEntity create (@RequestBody TaskModel taskModel, HttpServletRequest request){
         var idUser = request.getAttribute("idUser");
         taskModel.setIdUser((UUID) idUser);
-        var task = this.taskReposity.save(taskModel);
-        return task;
 
+        var currentDate = LocalDateTime.now();
+
+        if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt()) ){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de inicio / data de termino deve ser maior do que a data atual");
+        }
+
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de inicio deve ser maior que a data de termino");
+        }
+
+        var task = this.taskReposity.save(taskModel);
+        return ResponseEntity.status(HttpStatus.OK).body(task);
     }
 }
