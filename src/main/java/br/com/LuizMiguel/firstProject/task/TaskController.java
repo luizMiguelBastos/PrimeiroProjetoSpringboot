f0a9898a02f1bbabf6731e87046640cef1f5f75c
Luiz Miguel Bastos <luizmiguelbastos90@gmail.com>
2026-08-06 16:51:44 -0300

    Setando atributo no request para utilizar a propria autenticacao para conseguir o ID do usuario

--- src/main/java/br/com/LuizMiguel/firstProject/task/TaskController.java
+++ src/main/java/br/com/LuizMiguel/firstProject/task/TaskController.java
@@ -1,10 +1,13 @@
 package br.com.LuizMiguel.firstProject.task;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/tasks")
@@ -14,7 +17,9 @@ public class TaskController {
     private ITaskReposity taskReposity;
 
     @PostMapping("/")
-    public TaskModel create (@RequestBody TaskModel taskModel){
+    public TaskModel create (@RequestBody TaskModel taskModel, HttpServletRequest request){
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID) idUser);
         var task = this.taskReposity.save(taskModel);
         return task;
 
